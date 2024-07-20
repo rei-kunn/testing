50878aef9c9df9ce0306a5d03ba26eb52ee6fd6e
rei-kunn <reikamiya2016@gmail.com>
2024-07-20 09:25:21 +0900

    update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 def ans():
-    for i in range(1,100,2):
-        print(i)
-
+    ans = int(input("write any number from 1 to 10"))
+    if ans < 11:
+        for i in range(1,100,2):
+            print(i)
+    else:
+        raise ValueError("the value should be less than 11")
 ans()
